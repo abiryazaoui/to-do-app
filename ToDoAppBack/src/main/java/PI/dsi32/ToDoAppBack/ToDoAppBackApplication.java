d7f93c50d7b134ba26c366deb3004b6497ae123a
SyrineZahar <cyrine.zahaar@gmail.com>
2024-10-04 11:14:15 +0100

    back end app

---
+++ ToDoAppBack/src/main/java/PI/dsi32/ToDoAppBack/ToDoAppBackApplication.java
@@ -0,0 +1,13 @@
+package PI.dsi32.ToDoAppBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoAppBackApplication.class, args);
+	}
+
+}
---
+++ ToDoAppBack/src/test/java/PI/dsi32/ToDoAppBack/ToDoAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package PI.dsi32.ToDoAppBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
